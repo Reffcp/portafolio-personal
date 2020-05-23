64c02b4deab3b137acae621e232e8ddd56315614
Cristian Refugio Covarrubias Patiño <cristian.covarrubias@rebits.com.mx>
2020-05-22 19:04:27 -0500

    Terminado el desarrollo y maquetación. queda pendiente el formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 
+import { from } from 'rxjs';
+import { Router } from '@angular/router';
+import { link } from 'fs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  constructor(public router: Router ) {}
   ancla(id){
     document.getElementById(id).click();
+  }
+
+  gotoUrl(url){
+    window.open(url, '_blank');
   }
 }
