462745f12efe5e76b3a7b807f47dc44af7c2e315
daff <daffmg1@gmail.com>
2020-07-20 07:47:44 -0500

    menu de hamburguesa agregado

--- src/app/pages/nav-bar/nav-bar.component.ts
+++ src/app/pages/nav-bar/nav-bar.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit() {
+    
+  }
+
+  abrirMenu(){
+    var menu =document.querySelector('.menu')
+    if (menu.classList.contains('is-active')) {
+      menu.classList.remove('is-active')
+    } else {
+      menu.classList.add('is-active')
+    }
+  
   }
 
 }
