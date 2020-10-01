cd13bc15633425d1104e7ef8a51a82e7038adf9c
Cristian Refugio Covarrubias Patiño <cristian.covarrubias@rebits.com.mx>
2020-09-30 19:09:40 -0500

    hash agregado a las rutas para producción.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,{
-    preloadingStrategy: PreloadAllModules
+    preloadingStrategy: PreloadAllModules,
+    useHash: true
   })],
   exports: [RouterModule]
 })
