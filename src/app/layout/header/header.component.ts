350abc038254a5473df52cfdb5f1b3b72ad9c25c
reffcp <yo@cristiancovarrubias.online>
2022-08-16 17:32:20 -0500

    header del sitio v1

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { IMenuElement } from 'src/app/shared/models/menu-element.model';
+
+// menu: IMenuElement[] = [
+//   {
+//     name: 'Acerca',
+//     link: '/me',
+//   },
+//   {
+//     name: 'Experiencia Laboral',
+//     link: '/experience/jobs',
+//   },
+//   {
+//     name: 'Experiencia Academica',
+//     link: '/experience/study',
+//   },
+//   {
+//     name: 'Experiencia en Asesorias',
+//     link: '/experience/consulting',
+//   },
+//   {
+//     name: 'Proyectos Publicos',
+//     link: '/projects/public',
+//   },
+//   {
+//     name: 'Proyectos Privados',
+//     link: '/projects/private',
+//   },
+//   {
+//     name: 'Contacto',
+//     link: '/contact',
+//   },
+// ];
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  menu: IMenuElement[] = [
+    {
+      name: 'Inicio',
+      link: '/',
+    },
+    {
+      name: 'Experiencia Laboral',
+      link: '/experience',
+    },
+    {
+      name: 'Experiencia Academica',
+      link: '/experience',
+    },
+    {
+      name: 'Proyectos',
+      link: '/projects',
+    },
+    {
+      name: 'Contacto',
+      link: '/contact',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/app/shared/models/menu-element.model.ts
@@ -0,0 +1,4 @@
+export interface IMenuElement {
+  name: string;
+  link: string;
+}
