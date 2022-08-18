22e18725191bb704a33af336e536627396a24296
reffcp <yo@cristiancovarrubias.online>
2022-08-18 13:46:39 -0500

    se integra firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDKQ8slXsIb9ceMJ1OemkgiYH087kGbocI',
+    authDomain: 'reff-portafolio-personal.firebaseapp.com',
+    projectId: 'reff-portafolio-personal',
+    storageBucket: 'reff-portafolio-personal.appspot.com',
+    messagingSenderId: '964065404393',
+    appId: '1:964065404393:web:0f4e8d69d039af260dd778',
+    measurementId: 'G-ZHS551FT87',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDKQ8slXsIb9ceMJ1OemkgiYH087kGbocI',
+    authDomain: 'reff-portafolio-personal.firebaseapp.com',
+    projectId: 'reff-portafolio-personal',
+    storageBucket: 'reff-portafolio-personal.appspot.com',
+    messagingSenderId: '964065404393',
+    appId: '1:964065404393:web:0f4e8d69d039af260dd778',
+    measurementId: 'G-ZHS551FT87',
+  },
 };
 
 /*
