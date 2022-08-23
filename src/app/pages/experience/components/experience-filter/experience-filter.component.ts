0eeb39221fe45230685450d9e7ac117bca7b2bee
reffcp <yo@cristiancovarrubias.online>
2022-08-23 08:30:53 -0500

    avance vista de experiencias profesionales

--- src/app/pages/experience/components/experience-filter/experience-filter.component.ts
+++ src/app/pages/experience/components/experience-filter/experience-filter.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-experience-filter',
   templateUrl: './experience-filter.component.html',
-  styleUrls: ['./experience-filter.component.scss']
+  styleUrls: ['./experience-filter.component.scss'],
 })
 export class ExperienceFilterComponent implements OnInit {
-
-  constructor() { }
+  typeExperience = '';
+  typeExperienceTitle = '';
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.getParams();
   }
 
+  getParams(): void {
+    this.activatedRoute.params.subscribe((res: any) => {
+      this.typeExperience = res.type;
+      switch (res.type) {
+        case 'profesional':
+          this.typeExperienceTitle = 'Profesional';
+          break;
+        case 'freelancer':
+          this.typeExperienceTitle = 'Freelancer';
+          break;
+        case 'consultoria':
+          this.typeExperienceTitle = 'en Consultoría';
+          break;
+
+        default:
+          break;
+      }
+    });
+  }
 }
